c56ef80e4d4e6d70495120add959cd45add4c58d
lusine <nomenahaim1@gmail.com>
2026-02-13 23:16:25 +0400

    message

---
+++ ft_itoa.c
@@ -0,0 +1,45 @@
+#include "libft.h"
+
+static size_t	ft_numlen(long n)
+{
+	size_t	len;
+
+	len = 0;
+	if (n <= 0)
+		len++;
+	while (n != 0)
+	{
+		n = n / 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	long	nb;
+	size_t	len;
+	char	*str;
+
+	nb = n;
+	len = ft_numlen(nb);
+	str = malloc(len + 1);
+	if (!str)
+		return (NULL);
+	str[len] = '\0';
+	if (nb < 0)
+	{
+		str[0] = '-';
+		nb = -nb;
+	}
+	if (nb == 0)
+		str[0] = '0';
+	while (nb > 0)
+	{
+		len--;
+		str[len] = (nb % 10) + '0';
+		nb = nb / 10;
+	}
+	return (str);
+
+}	
